4c9ef8dda4deb3086c37e53dcc4d831fc004f138
Gabby Niamat <gniamat@uwo.ca>
2022-11-02 18:56:21 -0400

    fixed prisma client import errors

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,9 @@
-// import { Injectable } from '@nestjs/common';
-// import { PrismaClient } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../../node_modules/.prisma/client';
 
-// @Injectable()
-// export class PrismaService extends PrismaClient {
-//   constructor() {
-//     super();
-//   }
-// }
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super();
+  }
+}
