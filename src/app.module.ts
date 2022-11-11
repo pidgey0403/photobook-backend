58429a2bdd7f76b3cbb1feff5e568436bd62859b
Gabby Niamat <gniamat@uwo.ca>
2022-11-11 00:33:34 -0500

    added graphql and apollo-graphql to app.module.ts as dependency and module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql'], //specify where graphQL types should go
+      definitions: {
+        //schema first approach -> transform graphQL schema into ts classes
+        path: join(process.cwd(), 'src/types/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+    PrismaModule, //link PrismaModule to access our PrismaService
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
