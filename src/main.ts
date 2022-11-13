a581b95a3eddfc518826db109339b9f778b6ea43
Gabby Niamat <gniamat@uwo.ca>
2022-11-13 13:46:25 -0500

    fixing apollo server deployment

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const host = '0.0.0.0';
+  await app.listen(process.env.PORT, host);
 }
 bootstrap();
