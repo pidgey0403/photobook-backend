0180cbb7e8bb30e786375f11cd85c6864aaf557b
Gabby Niamat <gniamat@uwo.ca>
2022-11-18 17:00:03 -0500

    adding cors origin support to graphql and nestjs

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   const host = '0.0.0.0';
   await app.listen(process.env.PORT || 3001, host);
 }
