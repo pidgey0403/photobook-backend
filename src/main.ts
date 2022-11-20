1981dd5f3f0e1b506f23d8dc90d84c2edd1dff96
Gabby Niamat <gniamat@uwo.ca>
2022-11-20 14:04:56 -0500

    increasing JSON request body size

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@
 import { NestFactory } from '@nestjs/core';
 // import our App Module containing all controllers and providers required
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Allow for larger JSON request input sizes
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
